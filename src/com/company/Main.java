bc3bdbcbfec8675a3215efd9f6db73f04442f201
HS211777 <90605684+HS211777@users.noreply.github.com>
2021-09-27 09:19:59 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,20 @@ public class Main {
 	Scanner input = new Scanner(System.in);
 
     final double VAT = 0.2;
-    double userInput = input.nextDouble();
-    System.out.println("Enter the price without VAT: ");
-    userInput = Math.round(userInput*100.00)/100.00;
-    double tax = userInput * VAT;
-    tax = Math.round(tax*100.00)/100.00;
-    double output = tax + userInput;
-    System.out.println(output);
+    boolean end = false;
+    while (!end){
+        System.out.println("Enter the price without VAT: ");
+        double userInput = input.nextDouble();
+        userInput = Math.round(userInput*100.00)/100.00;
+        double tax = userInput * VAT;
+        tax = Math.round(tax*100.00)/100.00;
+        double output = tax + userInput;
+        System.out.println("£"+output);
+        System.out.println("go again? Y/N");
+        String repeat = input.next();
+        if (repeat.equalsIgnoreCase("n")){
+            end = true;
+        }
+    }
     }
 }
